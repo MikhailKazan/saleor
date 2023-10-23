00ba7d5859fbad54538b09760eeee5abee8813f6
mikhail <kazanmichail@hotmail.com>
2023-10-23 22:09:51 +0200

    Core Notifications Utils
    Adding functionality for filling the support contact data in the user emails

--- saleor/core/notification/utils.py
+++ saleor/core/notification/utils.py
@@ -5,6 +5,10 @@ from ..utils import build_absolute_uri
 
 LOGO_URL = "images/saleor-logo-sign.png"
 
+# TODO: SiteContext should be dynamical created
+SUPPORT_EMAIL = "email:support@enversio.com"
+SUPPORT_TELEFON_COUNTRY_CODE = "+49"
+SUPPORT_TELEFON_NUMBER = "12345-67-89"
 
 def get_site_context():
     site: Site = Site.objects.get_current()
@@ -12,5 +16,8 @@ def get_site_context():
         "domain": site.domain,
         "site_name": site.name,
         "logo_url": build_absolute_uri(staticfiles_storage.url(LOGO_URL)),
+        "support_email": SUPPORT_EMAIL,
+        "support_telefon_country_code": SUPPORT_TELEFON_COUNTRY_CODE,
+        "support_telefon_number": SUPPORT_TELEFON_NUMBER,
     }
     return site_context
