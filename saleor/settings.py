4739c4ec18a182ccab041325ca1d4b97ab1d4a06
mikhail <kazanmichail@hotmail.com>
2023-11-25 22:22:42 +0100

    adding live productive build ngrok domain to _DEFAULT_CLIENT_HOSTS in settings.py

--- saleor/settings.py
+++ saleor/settings.py
@@ -62,7 +62,7 @@ MANAGERS = ADMINS
 
 APPEND_SLASH = False
 
-_DEFAULT_CLIENT_HOSTS = "localhost,127.0.0.1,sdfaewfe7aodgfcaeut7ouasgb.eu.ngrok.io"
+_DEFAULT_CLIENT_HOSTS = "localhost,127.0.0.1,sdfaewfe7aodgfcaeut7ouasgb.eu.ngrok.io,c3e6305caff5.ngrok.app"
 
 ALLOWED_CLIENT_HOSTS = os.environ.get("ALLOWED_CLIENT_HOSTS")
 if not ALLOWED_CLIENT_HOSTS:
